00edf5a86c25d450e01353b6efe8d820d4a96adc
singingcactus <bechma02@gmail.com>
2019-11-29 12:38:13 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,18 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
+  accepts_nested_attributes_for :user, reject_if: :all_blank
   belongs_to :post
+
+  # def not_new(attributes)
+  #   byebug
+  #   attributes[:username].value.blank?
+  # end
+
+  # def user_attributes=(user_attributes)
+  #   new_user = User.find_or_create_by(user_attributes)
+  #   byebug
+  #   self.user = new_user
+  # end
+
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_attributes)
+    # byebug
+    categories_attributes.values.each do |category_attribute|
+      unless category_attribute.values[0].blank?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
+    end
+  end
 
 end
